bf59645af968886cc6f2039865d8a038d7b9d385
Jéssica Caroline <jessicacaroline3213@hotmail.com>
2022-11-01 00:22:06 -0300

    adiconado css e js

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+// SELEÇÃO DE ELEMENTOS
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+const searchInput = document.querySelector("#searchInput");
+const eraseBtn =  document.querySelector("#eraseBtn");
+const filterBtn = document.querySelector("#filterBtn");
+
+let oldInputValue;
+
+// FUNÇÕES
+const saveTodo = (text, done = 0, save = 1) => {
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = ' <i class="fa-solid fa-xmark"></i>';
+}
+
+// UTILIZANDO DADOS DA LOCALSTORAGE
