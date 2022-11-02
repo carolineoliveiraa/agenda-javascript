626a5a93adfe017200c504dfda13dc61cfc02abe
Jéssica Caroline <jessicacaroline3213@hotmail.com>
2022-11-01 21:32:29 -0300

    atualizd js

--- js/scripts.js
+++ js/scripts.js
@@ -32,6 +32,94 @@ const saveTodo = (text, done = 0, save = 1) => {
     const deleteBtn = document.createElement("button");
     deleteBtn.classList.add("remove-todo");
     deleteBtn.innerHTML = ' <i class="fa-solid fa-xmark"></i>';
-}
+
 
 // UTILIZANDO DADOS DA LOCALSTORAGE
+if (done) {
+    todo.classList.add("done");
+}
+
+if (save) {
+    saveTodoLocalStorage({text, done: 0})
+}
+
+todoList.appendChild(todo);
+
+todoInput.value = "";
+};
+
+const toggleForms = () => {
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide");
+    todoList.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {
+    const todos = document.querySelectorAll(".todo");
+
+    todos.forEach((todo) => {
+        let todoTitle = todo.querySelector("h3");
+
+        if(todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text;
+
+            // UTILIZANDO DADOS DA LOCALSTORAGE
+            updateTodoLocalStorage (oldInputValue, text);
+        }
+    });
+    
+    };
+
+    const getSearchedTodos = (search) => {
+        const todos = document.querySelector(".todo");
+
+        todos.forEach((todo) => {
+            const todoTitle = todo.querySelector("h3").innerText.toLowerCase();
+
+            todo.style.display = "flex";
+
+            console.log(todoTitle);
+
+            if(!todoTitle.includes(search)) {
+                todo.style.display = "none";
+            }
+        });
+    };
+
+    const filterTodos = (filterValue) => {
+        const todos = document.querySelectorAll(".todo");
+
+        switch(filterValue) {
+            case "all":
+                todos.forEach((todo) => (todo.style.display = "flex"));
+
+                break;
+
+            case "done":
+                todos.forEach((todo) =>
+                todo.classList.contains("done")
+                ? (todo.style.display = "flex")
+                : (todo.style.display = "none ")
+                );     
+
+                break;
+
+            case "todo":
+                todos.forEach((todo) =>
+                !todo.classList.contains("done")
+                ? (todo.style.display = "flex")   
+                : (todo.style.display = "none")   
+                );
+                
+                break;
+
+             default:
+                break;
+        }
+    };
+
+// EVENTOS
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+})
